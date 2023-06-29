cafe952f0156a36d3fdc147571c872988b72c030
AerlenTheStout <realtheyobros@gmail.com>
2023-06-29 20:45:00 -0300

    Added maybe way to save to disk

--- src/Commands/Fortune.js
+++ src/Commands/Fortune.js
@@ -3,12 +3,14 @@ const execSync = require('child_process').execSync;
 const { createCanvas, registerFont } = require('canvas');
 const fs = require('fs');
 
-function createPngImageFromString(text, width, height) {
+function createPngImageFromString(text) {
     // Create a canvas
     const canvas = createCanvas(width, height);
     const context = canvas.getContext('2d');
   
     // Draw the text on the canvas
+    let width = context.measureText(text).width; // Actual width of the text in pixels
+    let height = context.measureText(text).height; // Height of the text in pixels
     context.fillStyle = '#ffffff'; // Set the fill color
     context.fillRect(0, 0, width, height); // Fill the entire canvas with the background color
     context.font = '20px Arial'; // Set the font properties
@@ -17,9 +19,9 @@ function createPngImageFromString(text, width, height) {
   
     // Convert the canvas to a PNG buffer
     const buffer = canvas.toBuffer('image/png');
-  
+    //await fs.writeFile('fortune.png', buffer);
     return buffer;
-  }
+}
 
 
 module.exports = {
@@ -33,15 +35,9 @@ module.exports = {
         const output = execSync('fortune | cowsay', { encoding: 'utf-8' });  // the default is 'buffer'
         console.log('Output was:\n', output);
 
-        var imageString = output;
-        var witdth = 290;
-        var height = 290;
-
-
         await interaction.reply({
             content: 'Here is your fortune!',
-            attachment: [createPngImageFromString(imageString, witdth, height)]
-            
+            attachment: [new createPngImageFromString(output)]
         });
         
 
