f3189ef03ec2baedf22b622bb7e76350af7d5619
AerlenTheStout <realtheyobros@gmail.com>
2023-06-29 20:05:42 -0300

    To linux server for testing, made image creation

--- src/Commands/Fortune.js
+++ src/Commands/Fortune.js
@@ -1,5 +1,26 @@
 const { SlashCommandBuilder } = require('discord.js');
 const execSync = require('child_process').execSync;
+const { createCanvas, registerFont } = require('canvas');
+const fs = require('fs');
+
+function createPngImageFromString(text, width, height) {
+    // Create a canvas
+    const canvas = createCanvas(width, height);
+    const context = canvas.getContext('2d');
+  
+    // Draw the text on the canvas
+    context.fillStyle = '#ffffff'; // Set the fill color
+    context.fillRect(0, 0, width, height); // Fill the entire canvas with the background color
+    context.font = '20px Arial'; // Set the font properties
+    context.fillStyle = '#000000'; // Set the text color
+    context.fillText(text, 10, 50); // Draw the text at coordinates (10, 50)
+  
+    // Convert the canvas to a PNG buffer
+    const buffer = canvas.toBuffer('image/png');
+  
+    return buffer;
+  }
+
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -7,12 +28,17 @@ module.exports = {
         .setDescription('Get a fortune from the fortune cow!'),
 
     async execute(interaction) {
-        await interaction.reply('Moo!');
+        
 
-        const output = execSync('dir', { encoding: 'utf-8' });  // the default is 'buffer'
+        const output = execSync('fortune | cowsay', { encoding: 'utf-8' });  // the default is 'buffer'
         console.log('Output was:\n', output);
 
+        var imageString = output;
+        var witdth = 290;
+        var height = 290;
 
+        await interaction.reply(createPngImageFromString(imageString, witdth, height));
+        
 
     },
 };
