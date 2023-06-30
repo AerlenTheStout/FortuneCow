2d5c7c42014aa4932ae41addb6e7ce5ea4a00bbf
AerlenTheStout <realtheyobros@gmail.com>
2023-06-30 01:17:14 -0300

    Going global!

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -31,7 +31,7 @@ const rest = new REST().setToken(TOKEN);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
+			Routes.applicationCommands(clientId),
 			{ body: commands },
 		);
 
