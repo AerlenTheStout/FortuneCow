87254e8c00b4464105e29f6c6da9a5b3e839d0c3
AerlenTheStout <38132640+AerlenTheStout@users.noreply.github.com>
2023-07-06 23:59:33 +0000

    opting in adds name to json file, cant do more one

--- src/Commands/Crossroads-Opt-In.js
+++ src/Commands/Crossroads-Opt-In.js
@@ -1,23 +1,33 @@
 const { SlashCommandBuilder } = require('discord.js');
 const fs = require("node:fs");
+//get the json data and put into a variable
+const data = fs.readFileSync('./src/Crossroads.json')
+const jsonData = JSON.parse(data)
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('crossroads-opt-in')
-        .setDescription('Start walking around the crossroads! (Opt into getting crossroad fortunes)'),
+  data: new SlashCommandBuilder()
+    .setName('crossroads-opt-in')
+    .setDescription('Start walking around the crossroads! (Opt into getting crossroad fortunes)'),
 
-    async execute(interaction) {
-        await interaction.reply('Opted In! (Curretly under development aka no worky yet)');
-
-        jsonData.opted.push({
-            "id": interaction.user.id, 
-            "username": interaction.user.username}
-            )
-  
-          //write the new json data to the file
-          fs.writeFile('./src/Crossroads.json', JSON.stringify(jsonData), (err) => {
-            if (err) throw err;
-            console.log('Data written to file');
-          });
-    },
+  async execute(interaction) {
+    await interaction.reply('Opted In! (Curretly under development aka no worky yet)');
+    //check if the user is already opted in
+    //if they are, do nothing
+    //if they are not, add them to the json file
+    if (jsonData.opted.includes(interaction.user.id)) {
+      interaction.editReply('You are already opted in!')
+      return
+    } else {
+      jsonData.opted.push({
+        "id": interaction.user.id,
+        "username": interaction.user.username
+      }
+      )
+      //write the new json data to the file
+      fs.writeFile('./src/Crossroads.json', JSON.stringify(jsonData), (err) => {
+        if (err) throw err;
+        console.log('Data written to file');
+      });
+    }
+  },
 };
