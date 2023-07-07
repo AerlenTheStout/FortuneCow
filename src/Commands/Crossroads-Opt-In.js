46ad150f0bd37ea732d289bc149132575055fcb7
AerlenTheStout <38132640+AerlenTheStout@users.noreply.github.com>
2023-07-07 00:04:42 +0000

    finding real dirpath

--- src/Commands/Crossroads-Opt-In.js
+++ src/Commands/Crossroads-Opt-In.js
@@ -1,7 +1,8 @@
 const { SlashCommandBuilder } = require('discord.js');
 const fs = require("node:fs");
 //get the json data and put into a variable
-const data = fs.readFileSync('./src/Crossroads.json')
+const filePath = path.join(__dirname, "Crossroads.json");
+const data = fs.readFileSync(filePath)
 const jsonData = JSON.parse(data)
 
 module.exports = {
