dde1df5e8798e49a1ca05c0e47b34de9e77fbcc7
AerlenTheStout <38132640+AerlenTheStout@users.noreply.github.com>
2023-07-07 00:19:35 +0000

    still doiing path stuff

--- src/Commands/Crossroads-Opt-In.js
+++ src/Commands/Crossroads-Opt-In.js
@@ -2,8 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 const fs = require("node:fs");
 const path = require("node:path");
 //get the json data and put into a variable
-const relativePath = '../Crossroads.json'
-const filePath = path.resolve(relativePath)
+const filePath = path.resolve(__dirname, '../Crossroads.json')
 console.log(filePath)
 const data = fs.readFileSync(filePath)
 const jsonData = JSON.parse(data)
