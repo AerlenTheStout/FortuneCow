9912c11bcecfa2b422c2491c8d4d9496d71e60c3
AerlenTheStout <38132640+AerlenTheStout@users.noreply.github.com>
2023-07-07 00:38:42 +0000

    console.logs

--- src/FortuneCowPlus.js
+++ src/FortuneCowPlus.js
@@ -20,12 +20,14 @@ client.on("ready", (c) => {
 client.commands = new Collection();
 
 const commandsPath = path.join(__dirname, "Commands");
+console.log(commandsPath);
 const commandFiles = fs
   .readdirSync(commandsPath)
   .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
   const filePath = path.join(commandsPath, file);
+  console.log(filePath);
   const command = require(filePath);
   // Set a new item in the Collection with the key as the command name and the value as the exported module
   if ("data" in command && "execute" in command) {
