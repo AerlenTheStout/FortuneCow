9d48c1b6a71390c3af99225e3b82970413b06504
AerlenTheStout <38132640+AerlenTheStout@users.noreply.github.com>
2023-07-07 02:43:11 +0000

    new way of checking if includes

--- src/Commands/Crossroads-Opt-Out.js
+++ src/Commands/Crossroads-Opt-Out.js
@@ -15,18 +15,10 @@ module.exports = {
   async execute(interaction) {
     await interaction.deferReply();
 
-    try {
-      for (let i = 0; i < jsonData.opted.length; i++) {
-          if (jsonData.opted[i].id === interaction.user.id && jsonData.opted[i].username === interaction.user.username) {
-            spot = i
-            includes = true
-            break;
-          } 
-      }
-    }catch (error) {
-      console.error(error);
-      includes = false
-      }
+    if (jsonData.opted.some(e => e.id === interaction.user.id)) {
+      includes = true
+      spot = jsonData.opted.findIndex(e => e.id === interaction.user.id)
+    }
 
     if (includes = false) {
       interaction.editReply("You aren't opted in!")
